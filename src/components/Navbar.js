33ce92bd907676c7ebcfd6f61b55633bfa7b6a7a
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2022-01-01 02:37:31 -0600

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Style.css'
 import { Link } from 'react-router-dom'
 
 
 
 export default function Navbar() {
+    const links = [{ name: 'Home', to: '/' }, { name: 'Services', to: '/services' }, { name: 'Contact', to: '/contact' }];
+
+    const [menu, setMenu] = useState(false)
+
+    const showMenu = () => {
+        setMenu(!menu)
+    }
+
+    const closeMobileMenu = () => {
+        setMenu(false)
+    }
+
     return (
         <>
             <header className="header">
 
                 <a href="/" className="logo"> SearchPerk <i className="fab fa-searchengin"></i> </a>
 
-                <nav className="navbar">
+                <nav className="navbar" >
                     <Link to="/">home</Link>
                     <Link to="/services">services</Link>
                     <Link to="/contact">contact</Link>
                 </nav>
-                <div id="menu-btn" className="fas fa-bars"></div>
-                <div id="theme-btn" className="far fa-moon"></div>
 
+                <div className="menu-icon" onClick={showMenu}>
+                    <i className={menu ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+                <ul className={menu ? 'nav-menu active' : 'nav-menu'} >
+                    {links.map(item => (<Link to={item.to} className="nav-links" onClick={closeMobileMenu}>{item.name}</Link>))}
+                </ul>
                 <div className="scroll-indicator"></div>
             </header>
 
